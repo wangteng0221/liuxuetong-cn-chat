9e03bf911a070798b7cf4caa13f3f0714aff72a9
王腾 <131945720+wangteng0221@users.noreply.github.com>
2023-05-11 19:25:15 +0800

    Update sidebar.tsx
    修改侧边栏标题、副标题

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -119,9 +119,9 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+        <div className={styles["sidebar-title"]}>ChatGPT Liuxuetong</div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          Build  to go abroad to study AI Assistant.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
